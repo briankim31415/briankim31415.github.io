d706b57ecb2eb97abc1a6fead3d800b9cd4cd3e6
brian <briankim31415@gmail.com>
2024-09-02 14:32:55 -0500

    Personal ad for CS394N

---
+++ src/personal_ad/personal_ad.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('personal_ad')).render(
+	<React.StrictMode>
+		<body>
+			<h2>Brian Kim</h2>
+			Department of Electrical and Computer Engineering <br />
+			briankim31415@gmail.com
+			<p>
+				I am a second year master's student advised by Dr. Suzanne
+				Barber. My research area is custom user metrics for online
+				privacy policies. I also have background in data science and ML.
+				I have a hidden talent in drinking more coffee than I should be
+				drinking.
+			</p>
+			<p>Three potential topics I'd be interested in working are:</p>
+			<ul>
+				<li>Reinforcement Learning</li>
+				<li>LLMs</li>
+				<li>Computer Vision</li>
+			</ul>
+			<p>I have not teamed up with anyone yet.</p>
+		</body>
+	</React.StrictMode>
+);
